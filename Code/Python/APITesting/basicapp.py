49c2543e93677b6b3a083df4b90f9eadc04450b9
narquette <nicholas.arquette@gmail.com>
2020-03-28 08:51:05 -0500

    added single simple api test

---
+++ Code/Python/APITesting/basicapp.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, jsonify
+import numpy as np  
+from tensorflow.keras.models import load_model
+import joblib
+
+
+#### THIS IS WHAT WE DO IN POSTMAN ###
+# STEP 1: Create New Request
+# STEP 2: Select POST
+# STEP 3: Type correct URL (http://127.0.0.1:5000/api/flower)
+# STEP 4: Select Body
+# STEP 5: Select JSON
+# STEP 6: Type or Paste in example json request
+# STEP 7: Run 02-Basic-API.py to launch server and confirm the site is running
+# Step 8: Run API request
+
+def return_prediction(model,scaler,sample_json):
+    
+    # For larger data features, you should probably write a for loop
+    # That builds out this array for you
+    
+    sample_values = list(sample_json.values())
+        
+    appt = [sample_values]
+    
+    appt = scaler.transform(appt)
+    
+    classes = np.array([0,1])
+    
+    class_dict = {0:"No",1:"Yes"}
+    
+    class_ind = model.predict_classes(appt)
+    
+    resp = int(classes[class_ind][0])
+    
+    return (f"{resp} which is {class_dict.get(resp)}")
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>FLASK APP IS RUNNING!</h1>'
+
+
+# REMEMBER TO LOAD THE MODEL AND THE SCALER!
+flower_model = load_model("final_apptnoshow_model.h5")
+flower_scaler = joblib.load("apptnoshow_scaler.pkl")
+
+@app.route('/api/apptnoshow', methods=['POST'])
+def predict_flower():
+
+    content = request.json
+    
+    results = return_prediction(model=flower_model,scaler=flower_scaler,sample_json=content)
+    
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run()
